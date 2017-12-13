73556ee5996224a27267f091a660d5cfa5e1990d
shedoesthewoods <shedoesthewoods@users.noreply.github.com>
2017-12-14 00:02:34 +0300

    Update Main.java

--- dataproje3B/src/com/company/Main.java
+++ dataproje3B/src/com/company/Main.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String[] ürünler = { "Dizüstü Bilgisayar,Bilgisayar,Dell,XPS 13 9333 Intel Core i5,10,5799,5927.3",
+        String[] ürünler = {
+                "Dizüstü Bilgisayar,Bilgisayar,Dell,XPS 13 9333 Intel Core i5,10,5799,5927.3",
                 "Buzdolabı,Beyaz Eşya,Regal,Cool RGL 3000,45,899.5,929",
                 "Lazer Yazıcı,Yazıcı,HP,LaserJet Pro M102a G3Q34A,51,700,750",
                 "Cep Telefonu,Telefon ve Aksesuarlar,Asus,Zenfone 3 Max ZC553KL,29,1000,1400",
@@ -14,17 +15,14 @@ public class Main {
                 "Ocak,Beyaz Eşya,ICF,2817/443,9,341.8,388"};
 
         List<Ürün> liste = ürünNesneleriOluştur(ürünler);
-        Tree tree = new Tree();
-        TreeNode root = tree.getRoot();
+        Tree bst = binarySearchTree(liste);
 
-        Tree bts = binarySearchTree(liste);
-        List<Tree> kategoriAğaçları = kategoriAğacı(liste, kategoriler(liste));
-//        System.out.println("Ağacın derinliği: " + kategoriAğaçları.get(0).depth(root));
-        //kategori ağaçlarının özelliklerini tek tek yazdıracak metodları yaz sonra
-
+        List<String> kategoriler = kategoriler(liste);
+        List<Tree> kategoriAğaçları = kategoriAğacı(liste, kategoriler);
 
 
     }
+
     //Verilen ürün stringlerini Ürün nesnelerine çevirip listeye yerleştiren metod
     public static List<Ürün> ürünNesneleriOluştur(String[] ürünler){
         List<Ürün> liste = new ArrayList<Ürün>();
@@ -56,14 +54,17 @@ public class Main {
     }
 
     //Kategori ağaçlarını sıralanmış kategori isimleriyle uyumlu indexlerde başka bir listeye atan metod
-    public static List<Tree> kategoriAğacı(List<Ürün> list, List<String> kategoriler){ //hata var dön
+    public static List<Tree> kategoriAğacı(List<Ürün> list, List<String> kategoriler){
         List<Tree> kategoriAğaçları = new ArrayList<>();
+        Tree tempTree;
 
         for (int i = 0; i < kategoriler.size(); i++) {
+            tempTree = new Tree();
             for (Ürün ürün : list){
                 if (ürün.getKategori().equals(kategoriler.get(i)))
-                    kategoriAğaçları.get(i).insert(ürün);
+                    tempTree.insert(ürün);
             }
+            kategoriAğaçları.add(tempTree);
         }
         return kategoriAğaçları;
     }
@@ -72,7 +73,8 @@ public class Main {
     public static List<String> kategoriler(List<Ürün> list){
         List<String> isimler = new ArrayList<>();
         for (Ürün ürün : list)
-            isimler.add(ürün.getKategori());
+            if(!isimler.contains(ürün.getKategori()))
+                isimler.add(ürün.getKategori());
 
         Collections.sort(isimler);
         return isimler;
@@ -80,16 +82,19 @@ public class Main {
 
     //Verilen bir kategoriyi verilen başka bir kategoriye ekleyen metod
     public static void kategoriAktar(String nereden, String nereye, List<Tree> kategoriAğaçları){
+        /**
+         *
+         * yine bak olmamış
+         */
         List<TreeNode> tempList = new ArrayList<>();
         TreeNode tempRoot;
         for(Tree tree : kategoriAğaçları){
             tempRoot = tree.getRoot();
             if (tempRoot.data.getKategori().equals(nereden)) {
                 while (!tree.isEmpty())
-                    tempList.add(tree.delete(tempRoot, tempRoot.data.toString()));
+                    tempList.add(tree.delete(tempRoot.data.toString()));
             }
         }
-
         for(Tree tree : kategoriAğaçları){
             tempRoot = tree.getRoot();
             if (tempRoot.data.getKategori().equals(nereye)){
@@ -117,4 +122,28 @@ public class Main {
                 ürün.setFiyat(ürün.getFiyat()*0.9);
         }
     }
+
+    //Bir ağaçtaki tüm ürünler satıldığında elde edilecek karı hesaplayan metod
+    //Kar hesabı yapılacak ağacın kök düğümü parametre olarak verilmelidir
+    public static void karHesapla(TreeNode node, double kar){
+        if (node == null) return;
+        kar += Math.abs(node.data.getFiyat() - node.data.getMaliyet()) * node.data.getMiktar();
+        karHesapla(node.left, kar);
+        karHesapla(node.right, kar);
+    }
+
+    //Ürünlerin maliyetlerini heap veri yapısına yerleştiren metod
+    public static void maliyetHeap(Heap heap, List<Ürün> liste){
+        for(Ürün ürün : liste)
+            heap.insert(ürün.getMaliyet());
+    }
+
+    //Ürünler arasında en ucuz ve pahalı iki ürünü listeleyen metod
+    public static void ucuzPahalı(Heap heap){
+        System.out.println("Yığındaki en pahalı ürün: " + heap.remove());
+        System.out.println("Yığındaki en pahalı ikinci ürün: " + heap.remove());
+        System.out.println("Yığındaki en ucuz ürün: " + heap.removeMin());
+        System.out.println("Yığındaki en ucuz ikinci ürün: " + heap.removeMin());
+    }
+
 }
